7a208dd6b0375ad5de6a0f99e251c463baad2897
PatoAnastacio207 <pcnc2013@gmail.com>
2020-08-12 03:49:07 -0300

    Gui start
    Started implementing the gui for the youtube player

---
+++ gui.py
@@ -0,0 +1,11 @@
+import tkinter
+import youtube_search
+
+ventana = tkinter.Tk()
+ventana.geometry("420x420")
+
+
+boton = tkinter.Button(ventana, text="SDF", command=)
+boton.pack()
+
+ventana.mainloop()
--- youtube_search.py
+++ youtube_search.py
@@ -3,43 +3,48 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-search_url = ""
-search_str = ""
 
-sys.argv[0] = ""
+if __name__ == "__main__":
+    search_url = ""
+    search_str = ""
 
-try:
-    for i in sys.argv:
-        search_str += i
-        search_str += " "
-    search_url = 'https://www.youtube.com/results?search_query=' + search_str
-except:
-    search_str = input("->  ")
-    search_url = 'https://www.youtube.com/results?search_query=' + search_str
+    sys.argv[0] = ""
 
-
-youtube_driver = webdriver.Chrome()
-youtube_driver.maximize_window()
-youtube_driver.get(search_url)
+    try:
+        for i in sys.argv:
+            search_str += i
+            search_str += " "
+        search_url = 'https://www.youtube.com/results?search_query=' + search_str
+    except:
+        search_str = input("->  ")
+        search_url = 'https://www.youtube.com/results?search_query=' + search_str
 
 
-while True:
-    
-    search_url = 'https://www.youtube.com/results?search_query=' + search_str
-
-
+    youtube_driver = webdriver.Chrome()
+    youtube_driver.maximize_window()
     youtube_driver.get(search_url)
 
 
+    while True:
+        
+        search_url = 'https://www.youtube.com/results?search_query=' + search_str
 
-    video = youtube_driver.find_element_by_xpath('//*[@id="contents"]/ytd-video-renderer[1]')
 
-    video.click()
+        youtube_driver.get(search_url)
 
-    search_str = input("->  ")
-    usr_inp = input("enter for next video")
-    if usr_inp == "0":
-        break
-    
 
-youtube_driver.close()
+
+        video = youtube_driver.find_element_by_xpath('//*[@id="contents"]/ytd-video-renderer[1]')
+
+        video.click()
+
+        search_str = input("->  ")
+        usr_inp = input("enter for next video")
+        if usr_inp == "0":
+            break
+        
+
+    youtube_driver.close()
+
+
+#/html/body/ytd-app/div/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[1]/div[1]/div/div[1]/div/div/ytd-menu-renderer/yt-icon-button
