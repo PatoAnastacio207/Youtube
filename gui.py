c2617832a584ef9c76b2d766768407314611e8ee
PatoAnastacio207 <pcnc2013@gmail.com>
2020-08-12 05:07:36 -0300

    Basic gui working
    Text box and button working

--- gui.py
+++ gui.py
@@ -1,11 +1,25 @@
 import tkinter
 import youtube_search
 
+def prueba(arg):
+	print(arg)
+
+# youtube_search.start_window()
+
 ventana = tkinter.Tk()
 ventana.geometry("420x420")
 
+text_box = tkinter.Entry(ventana)
+text_box.pack()
 
-boton = tkinter.Button(ventana, text="SDF", command=)
+
+
+def get_text_box():
+	txt = text_box.get()
+	text_box.insert(0, "")
+	return txt
+
+boton = tkinter.Button(ventana, text="Buscar", command= lambda: youtube_search.play_song(get_text_box()))
 boton.pack()
 
 ventana.mainloop()
